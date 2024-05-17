babc9c5894ee3e2e7fe1184effa48db751819c5d
k16 phideptraivodichvutru <phidoan5153@gmail.com>
2024-05-17 19:28:31 +0700

    add new gitignore

--- UniProjectHub-BE/UniProjectHub-BE/obj/Debug/net8.0/UniProjectHub-BE.AssemblyInfo.cs
+++ UniProjectHub-BE/UniProjectHub-BE/obj/Debug/net8.0/UniProjectHub-BE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniProjectHub-BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1876d5e9c6779556299c8ac518b848f44431e2d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniProjectHub-BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniProjectHub-BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
