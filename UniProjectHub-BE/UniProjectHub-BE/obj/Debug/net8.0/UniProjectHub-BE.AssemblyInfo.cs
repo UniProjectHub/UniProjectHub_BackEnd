e0a99b03a5f6366e468d3533dc5fe0f67255cb3e
AnBinh1703 <dan1314705@gmail.com>
2024-06-01 15:28:56 +0700

    update
    update

--- UniProjectHub-BE/UniProjectHub-BE/obj/Debug/net8.0/UniProjectHub-BE.AssemblyInfo.cs
+++ UniProjectHub-BE/UniProjectHub-BE/obj/Debug/net8.0/UniProjectHub-BE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniProjectHub-BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7093cae54d0cf2a26dceeca6e58a55acb1d130ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90befffc2d7e2676f0cd1bde39fb7d610762cadf")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniProjectHub-BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniProjectHub-BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
