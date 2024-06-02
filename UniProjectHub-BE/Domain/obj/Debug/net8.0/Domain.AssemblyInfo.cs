2f3e772ce28024b90524c2f2fc3475b41f208582
Cong Cao <caocong2404@gmail.com>
2024-06-02 23:34:15 +0700

    Commit

--- UniProjectHub-BE/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ UniProjectHub-BE/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16619c9fecfb9ed16bda58c907d05d1beb5b7b28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d28f97afefe89d52643f4cfbe6a203e843b53085")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
