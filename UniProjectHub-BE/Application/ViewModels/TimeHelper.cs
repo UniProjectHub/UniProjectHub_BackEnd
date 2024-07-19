1cf91c6997e06c7377db37c39eb1d4dacb420e3a
k16 phideptraivodichvutru <phidoan5153@gmail.com>
2024-07-20 00:35:01 +0700

    update: DateTime

---
+++ UniProjectHub-BE/Application/ViewModels/TimeHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModels
+{
+    public static class TimeHelper
+    {
+        public static DateTime GetVietnamTime()
+        {
+            var vietnamTimeZone = TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time");
+            return TimeZoneInfo.ConvertTime(DateTime.Now, vietnamTimeZone);
+        }
+    }
+}
